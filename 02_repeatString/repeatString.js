b6635cf6c3e5e16f1a5b47da72168b58ae57acee
Sekiei <taimur.shabbir@gmail.com>
2022-06-02 23:19:29 +0500

    Repeating challanges for tests which were earlier skipped!!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,17 @@
 const repeatString = function(string, times) {
-    concatString = string;
-    for (i=0; i<times-1; i++) {
-        concatString = concatString + string;
+    if (times <= -1) {
+        return "ERROR";
     }
-    return concatString;
+    else if (times == 0) {
+        return "";
+    }
+    else {
+        concatString = string;
+        for (i=0; i<times-1; i++) {
+            concatString = concatString + string;
+        }
+        return concatString;
+    }
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,6 +7,23 @@ const subtract = function(num1, num2) {
 };
 
 const sum = function(num1, num2) {
+  if (num1 < num2) {
+    sum = num1;
+    for (i=num1 + 1; i <= num2; i++) {
+        sum = sum + i; 
+    }
+    return sum;
+}
+else if (num2 < num1) {
+    sum = num2;
+    for (i=num2 + 1; i <= num1; i++) {
+        sum = sum +i;
+    }
+    return sum;
+}
+else {
+    return num1;
+}
 };
 
 const multiply = function(num1, num2) {
