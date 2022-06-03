847a3032fe58eeb3461e4bf2bd602c6bef1ee055
Sekiei <taimur.shabbir@gmail.com>
2022-06-03 12:32:11 +0500

    Challange 4-5 All Tests Passes!!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,7 @@
 const removeFromArray = function(array, item1, item2, item3, item4, item5) {
     for (i=0; i < array.length; i++) {
-        if (array[i] == item1 || array[i] == item2 || array[i] == item3
-            || array[i] == item4 || array[i] == item5) {
+        if (array[i] === item1 || array[i] === item2 || array[i] === item3
+            || array[i] === item4 || array[i] === item5) {
                 array.splice(i, 1);
                 i = -1;
             }
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,20 +1,30 @@
 const sumAll = function(num1, num2) {
-    if (num1 < num2) {
-        sum = num1;
-        for (i=num1 + 1; i <= num2; i++) {
-            sum = sum + i; 
+    if (typeof num1 == 'number' && typeof num2 == 'number') {
+        if (num1 >= 0 && num2 >= 0) {
+            if (num1 < num2) {
+                sum = num1;
+                for (i=num1 + 1; i <= num2; i++) {
+                    sum = sum + i; 
+                }
+                return sum;
+            }
+            else if (num2 < num1) {
+                sum = num2;
+                for (i=num2 + 1; i <= num1; i++) {
+                    sum = sum +i;
+                }
+                return sum;
+            }
+            else {
+                return num1;
+            }
         }
-        return sum;
-    }
-    else if (num2 < num1) {
-        sum = num2;
-        for (i=num2 + 1; i <= num1; i++) {
-            sum = sum +i;
+        else {
+            return "ERROR";
         }
-        return sum;
     }
     else {
-        return num1;
+        return "ERROR";
     }
 };
 
