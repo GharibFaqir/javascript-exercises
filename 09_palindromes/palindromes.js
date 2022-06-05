23610c18af622d0debb24ed472c75e953dfea719
Sekiei <taimur.shabbir@gmail.com>
2022-06-05 13:45:48 +0500

    Challange 09: App Tests Passed!! (Y)

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    console.log(str);
+    str = str.replace(/[.?!,;:_'"(){} ]/g,'');
+    str = str.toLowerCase();
+    console.log(str); 
+    j = str.length - 1; 
+    
+    for (i=0; i <= (Math.round(str.length/2))-1; i++) {
+        if (str[i] == str[j]) {
+            j = j-1;
+        }
+        else {
+            return false
+        }
+    }
+    if (i == (Math.round(str.length/2))) {
+        return true;
+    }
 };
+
+console.log(palindromes('selles'));
+// palindromes(`A !quick, ;brown: fox_ 'jumped" (over) {the} .fence?`);
 
 // Do not edit below this line
 module.exports = palindromes;
