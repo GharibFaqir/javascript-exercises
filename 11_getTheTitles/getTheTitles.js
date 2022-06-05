50490383ee5c445ef929d60d1a87247d52c176eb
Sekiei <taimur.shabbir@gmail.com>
2022-06-05 17:22:05 +0500

    Challange 11: All tests passed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,12 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    title = [];
+    for (i=0; i <= books.length-1; i++) {
+        title[i] = books[i].title;
+    }
+    return title;
 };
 
+books = [{title: 'book1', author: 'name1'}, {title: 'book2', author: 'name2'}];
+getTheTitles(books);
 // Do not edit below this line
 module.exports = getTheTitles;
